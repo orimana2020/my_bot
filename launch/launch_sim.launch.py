eb978b79607a24911d779e950cfd985b1335598c
Ori Man <ori.mana@gmail.com>
2023-04-26 19:42:53 +0300

    teleop

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,47 +1,78 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
-
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
+    
     package_name='my_bot' #<--- CHANGE ME
+
+    pkg_share = FindPackageShare(package='my_bot').find('my_bot')
+    world_file_name = 'obstacles.world'
+    world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+    declare_world_cmd = DeclareLaunchArgument(
+        name='world',
+        default_value=world_path,
+        description='Full path to the world model file to load')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                    get_package_share_directory(package_name),'launch','rsp.launch.py')])
+                , launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+                    launch_arguments={'extra_gazebo_args': '--ros-agrs --params-file'+gazebo_params_file}.items()
+            )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
+    diff_drive_spawner = Node(package="controller_manager", executable="spawner", arguments=["diff_cont"])
+    joint_broad_spawner = Node(package="controller_manager", executable="spawner", arguments=["joint_broad"])
 
+    # RVIZ
+    rviz = Node(
+            package='rviz2',
+            namespace='',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', [os.path.join(pkg_share, 'config', 'my_bot.rviz')]]
+        )
+
+    #twist
+    # twist_stamper = Node(
+    #         package='teleop_twist_keyboard',
+    #         executable='teleop_twist_keyboard',
+    #         name='teleop',
+    #         output='screen',
+    #         remappings=[('/cmd_vel', '/diff_cont/cmd_vel_unstamped')]
+    #     )
 
 
     # Launch them all!
     return LaunchDescription([
+        declare_world_cmd,
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        rviz,
+        # twist_stamper,
     ])

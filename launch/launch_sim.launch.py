24dd73c730374106f0950c88868c2fa9af97346b
Ori Man <ori.mana@gmail.com>
2023-05-31 17:07:51 +0300

    remove ros2_control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -45,29 +45,15 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
     
-    diff_drive_spawner = Node(package="controller_manager", executable="spawner", arguments=["diff_cont"])
-    joint_broad_spawner = Node(package="controller_manager", executable="spawner", arguments=["joint_broad"])
-
-    # Rviz
-    rviz_path='src/my_bot/config/my_bot.rviz'
-    rviz2 = Node(package='rviz2', executable='rviz2', 
-                 name="rviz2", output='screen',
-                   arguments=['-d'+str(rviz_path), {'use_sim_time': 'true'}])
-        
-    # mappiung
-    # online = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','online_async.launch.py')])
-    #             , launch_arguments={'use_sim_time': 'true'}.items() )
-
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
-    delayed_rviz2 = TimerAction(period=7.0, actions=[rviz2])
+  
+    # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    # twist_mux = Node(
+    #         package="twist_mux",
+    #         executable="twist_mux",
+    #         parameters=[twist_mux_params],
+    #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    #     )
+    # delayed_rviz2 = TimerAction(period=10.0, actions=[rviz2])
 
     # Launch them all!
     return LaunchDescription([
@@ -75,14 +61,10 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner,
-        delayed_rviz2,
-        twist_mux,
     ])
 # Run the following:
-
 # ros2 launch my_bot launch_sim.launch.py 
+# ros2 run rviz2 rviz2 --ros-args -p use_sim_time:=true
 # ros2 launch my_bot online_async_launch.py use_sim_time:=true     
 # ros2 launch nav2_bringup localization_launch.py map:=./my_map_save.yaml use_sim_time:=true 
 # ros2 launch nav2_bringup navigation_launch.py use_sim_time:=true map_subscribe_transient_local:=true
@@ -90,11 +72,3 @@ def generate_launch_description():
 # ros2 run teleop_twist_keyboard teleop_twist_keyboard 
 # --------------------------------------------------------
 
-# mapping 
-
-
-
-# no need to launch
-# ros2 run rviz2 rviz2 -d src/my_bot/config/my_bot.rviz --ros-args -p use_sim_time:=true
-# ros2 run twist_mux twist_mux --ros-args --params-file ./src/my_bot/config/twist_mux.yaml -r cmd_vel_out:=diff_cont/cmd_vel_unstamped
-# ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstamped
